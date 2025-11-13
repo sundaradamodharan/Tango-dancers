60d70fc85f317395eaf7b93288cf385fd4b23593
SUNDARA DAMODHARAN M K <91125510+sundaradamodharan@users.noreply.github.com>
2025-11-13 11:15:23 +0530

    Update description with game rules
    Added rules for the dance challenge in the description.

--- src/App.js
+++ src/App.js
@@ -157,7 +157,11 @@ export default function App() {
   return (
     <div className="app">
       <h1>💃🕺 Tango Logic Puzzle</h1>
-      <p>Each refresh brings a new dance challenge!</p>
+      <p>Each refresh brings a new dance challenge!
+    Rules:1) Each row and colum must contain equal number of charaters
+          2) In the same row there must not be more than 2 characters concecutively!!
+          3) This is just a game play simple and be Happy !!!
+    </p>
 
       <div className="grid">
         {grid.map((row, r) =>
